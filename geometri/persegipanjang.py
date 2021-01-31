5e20c7258642972e5fb137dfe466c011f7a291a1
Reza Mahendra Dhata <rezamahendradhata@gmail.com>
2021-01-31 23:00:38 +0700

    initial commit

---
+++ geometri/persegipanjang.py
@@ -0,0 +1,12 @@
+class PersegiPanjang():
+    def __init__(self,p, l):
+        # fungsi yang di panggil pertama kali saat object diciptakan
+        self.p = p
+        self.l = l
+
+    def info (self):
+        return f'ini adaalah object persegi panjang dengan panjang = {self.p} dan lebar = {self.l}'
+
+    def hitung_luas(self):
+        return self.p * self.l
+
