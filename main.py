3a1078a0d5f4bfde804aacddb85b04df15cd7718
Reza Mahendra Dhata <rezamahendradhata@gmail.com>
2021-02-01 21:25:44 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bangunruang import BangunRuang
 from geometri.persegipanjang import PersegiPanjang
 from geometri.segitiga import Segitiga
 
@@ -9,3 +10,8 @@ print (p1.hitung_luas())
 s1 = Segitiga (4,2)
 print (s1.info())
 print (s1.hitung_luas())
+
+print ('Mencoba membuat object dari BangunRuang')
+b1 = BangunRuang()
+print(b1.info())
+print (b1.hitung_luas())
